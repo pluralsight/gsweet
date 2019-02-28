4698c9076f2e41a7d9ead416c6806bf636a21253
Tod Gentille <tod.gentille@gmail.com>
2019-02-27 18:29:30 -0800

    working

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 const ss = require("./sheetService");
+const sheetOps = require("./sheetOps");
+
+const sampleSpreadsheetId = "1UYSrzhP0hNYViJITcMuU5FY_CUhPxkGEEHfE59_meM8";
 
 const main = async () => {
   console.log("Hello World.");
   const sheetService = ss.init();
-
+  sheetOps.init(sheetService);
+  const values = await sheetOps.getSheetValues(sampleSpreadsheetId, "Learning Blueprint!P2:P67");
+  console.log(values);
 };
 
 main();
