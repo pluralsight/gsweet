9c1fb1d316964dd8d71406366855d7e24d2050c1
Tod Gentille <tod.gentille@gmail.com>
2019-03-05 20:46:39 -0800

    clean up

--- src/main.js
+++ src/main.js
@@ -1,53 +1,6 @@
-const driveOps = require("./Drive/driveOps");
-const sheetOps = require("./sheets/sheetOps");
+const driveOps = require("./Drive/driveOps")
+const sheetOps = require("./sheets/sheetOps")
 module.exports = {
   driveOps,
   sheetOps,
-};
-
-// const main = async () => {
-//   logger.silly("Hello World.");
-//   const sheetService = ss.init();
-//   sheetOps.init(sheetService);
-//   // const values = await sheetOps.getSheetValues(sampleSpreadsheetId, "Learning Blueprint!P2:P67");
-//   // logger.info(JSON.stringify(values));
-
-//   const _driveService = driveService.init();
-//   driveOps.init(_driveService);
-
-//   const LB_FOLDER_ID = "1svR6YuJIfkfJZEDb9XTlt82s57Kagbxg";
-//   // const READY_DB_ID = "17ztaxi-kM0RYk-XG3vUFKloExewpFLeM";
-//   const root = "root";
-
-
-//   const files = await driveOps.getFilesInFolder(root, driveOps.mimeType.FOLDER)
-//     .catch(error => {
-//       logger.debug(error);
-//     });
-//   // let names = files.map(e => e.name);
-//   logger.info(JSON.stringify(files.map(e => e.name)));
-//   logger.debug("----------");
-
-//   const moreFiles = await driveOps.getFilesInFolder(LB_FOLDER_ID, driveOps.mimeType.FOLDER)
-//     .catch(error => {
-//       logger.debug(error);
-//     });
-//   logger.info(moreFiles.map(e => e.name));
-//   logger.debug("----------");
-//   logger.silly("======");
-
-// const dbFiles = await driveOps.getFilesInFolderId(READY_DB_ID, driveOps.mimeType.FOLDER)
-//   .catch(error => {
-//     logger.debug(error);
-//   });
-// names = dbFiles.map(e => e.name);
-// logger.info(JSON.stringify(names));
-
-
-// await driveOps.countAllFiles()
-//   .catch(error => {
-//     logger.debug(error);
-//   });
-// };
-
-// main();
+}
