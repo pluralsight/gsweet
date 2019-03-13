4b2b1e379f18e7448210635e0fb74f2c16ae9ce2
Tod Gentille <tod.gentille@gmail.com>
2019-03-12 20:11:35 -0700

    change to class

--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
-const envCreate = require("env-create")
-const driveOps = require("./Drive/driveOps")
-const sheetOps = require("./sheets/sheetOps")
+const envCreate = require('env-create')
+const driveOps = require('./Drive/driveOps')
+const sheetOps = require('./sheets/sheetOps')
+const authHelper = require('./googleAuthHelper')
 
 const auth = (path) => {
-  console.log("running auth", path)
+  console.log('running auth', path)
   envCreate.load({path}) // make path into an object 
 }
-module.exports = {
-  auth,
-  driveOps,
-  sheetOps,
+class GSweet {
+  constructor(pathString) {
+    envCreate.load({path: pathString})
+    this.sheetOps = sheetOps
+    this.driveOps = driveOps
+    this.sheetOps.autoInit()
+    this.driveOps.autoInit()
+
+  }
 }
+
+module.exports = GSweet
+// module.exports = {
+//   auth,
+//   driveOps,
+//   sheetOps,
+// }
 
 // This is how you could return both a function that runs and some properties
 // however when I did this I lost the intellisense on driveOps and sheetOps
